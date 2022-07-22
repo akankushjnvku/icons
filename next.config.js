58e8071fef1b0ade498de9ad6a4095e3f88f887a
Richard McCartney <98745475+richardmccartney@users.noreply.github.com>
2022-07-22 11:03:16 +0100

    chore: resolving demo build issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const package = require('./package.json');
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? '/icons/branch/ve' : '',
+  swcMinify: true,
+  assetPrefix: isProd ? '/icons' : '',
   publicRuntimeConfig: {
     libVersion: `v${package.version}`,
   },
